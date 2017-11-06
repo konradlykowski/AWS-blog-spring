410a1a209e06725a32ddfd90d09dfaa60abaadca
Konrad Lykowski <konrad.lykowski@outlook.com>
2017-11-06 11:23:05 +0100

    add new functionality

---
+++ src/main/java/com/konrad/examples/blog/Application.java
@@ -0,0 +1,11 @@
+package com.konrad.examples.blog;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class Application {
+
+    static void main(String[] args) {
+        SpringApplication.run(Application.class, args)
+    }
+}
